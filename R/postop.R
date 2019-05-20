09e4ea6f6775062433f4b451e8c9dfb4d87904e1
Talbot <talbot.steven@mh-hannover.de>
2019-05-20 19:13:11 +0200

    Add transmitter postop data for external use.

---
+++ R/postop.R
@@ -0,0 +1,18 @@
+#' Transmitter operated mice
+#'
+#' Data from a transmitter implantation experiment with various behavioural data. Also includes sham-operated animals.
+#' Observation time was over 30 days, starting with day=-1 as baseline (physiological state). The treatment columns shows
+#' what operational state the animal is in and the condition field shows two possible types of analgesia.
+#'
+#' @docType data
+#'
+#' @usage data(postop)
+#'
+#' @format A data frame with post-op data.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(postop)
+#' head(postop)
+"postop"
